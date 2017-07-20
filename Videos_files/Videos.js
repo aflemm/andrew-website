5d1d8ddb6e730f7677f99c5a0c440ba31a46b677
Andrew Flemming <aflemm@users.noreply.github.com>
2017-07-20 13:23:38 -0400

    Initial commit
    initila commit

---
+++ Videos_files/Videos.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20131113
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:0,offset:new IWPoint(-0.0000,1.0000),color:'#ffffff',opacity:1.000000}),shadow_3:new IWShadow({blurRadius:0,offset:new IWPoint(-0.0000,1.0000),color:'#ffffff',opacity:1.000000}),shadow_7:new IWShadow({blurRadius:0,offset:new IWPoint(-0.0000,1.0000),color:'#ffffff',opacity:1.000000}),shadow_8:new IWShadow({blurRadius:0,offset:new IWPoint(-0.0000,1.0000),color:'#ffffff',opacity:1.000000}),shadow_0:new IWShadow({blurRadius:0,offset:new IWPoint(-0.0000,1.0000),color:'#ffffff',opacity:1.000000}),shadow_6:new IWShadow({blurRadius:0,offset:new IWPoint(-0.0000,1.0000),color:'#ffffff',opacity:1.000000}),shadow_5:new IWShadow({blurRadius:0,offset:new IWPoint(-0.0000,1.0000),color:'#ffffff',opacity:1.000000}),shadow_4:new IWShadow({blurRadius:0,offset:new IWPoint(-0.0000,1.0000),color:'#ffffff',opacity:1.000000}),shadow_1:new IWShadow({blurRadius:0,offset:new IWPoint(-0.0000,1.0000),color:'#ffffff',opacity:1.000000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Videos_files/VideosMoz.css')
+detectBrowser();adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');IMpreload('Videos_files','shapeimage_1','0');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
