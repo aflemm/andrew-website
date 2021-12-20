356a9f20152d8e827503e33ca9a83579682e0778
aflemm <aflemm@users.noreply.github.com>
2021-12-20 19:52:30 +0000

    Published: Output Clean

---
+++ docs/archive/teamflemming/Regionals_files/Regionals.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20131030
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,986),url:'Regionals_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Regionals_files/stroke_1.png'},{rect:new IWRect(2,-2,693,4),url:'Regionals_files/stroke_2.png'},{rect:new IWRect(695,-2,5,4),url:'Regionals_files/stroke_3.png'},{rect:new IWRect(695,2,5,986),url:'Regionals_files/stroke_4.png'},{rect:new IWRect(695,988,5,4),url:'Regionals_files/stroke_5.png'},{rect:new IWRect(2,988,693,4),url:'Regionals_files/stroke_6.png'},{rect:new IWRect(-2,988,4,4),url:'Regionals_files/stroke_7.png'}],new IWSize(697,990))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Regionals_files/RegionalsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id6');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
